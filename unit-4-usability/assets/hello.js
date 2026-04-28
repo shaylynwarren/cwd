d34b26da053fd07772d2c6702bf2da6387ce832f
shaylynwarren <shaylyncwarren@gmail.com>
2026-04-28 08:24:50 -0400

    updates?

---
+++ unit-4-usability/assets/hello.js
@@ -0,0 +1,10 @@
+
+let myForm = document.querySelector("#myForm");
+
+myForm.addEventListener("submit" , (event) => {
+	event.preventDefault();
+
+
+// console.log("Hello");
+	console.log(this.msg.value)
+});
