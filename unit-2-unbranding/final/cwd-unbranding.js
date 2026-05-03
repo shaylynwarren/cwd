d4ad1580787f413d81b7d12b64bbecfe392bc1ee
shaylynwarren <shaylyncwarren@gmail.com>
2026-05-02 22:37:21 -0400

    trying

---
+++ unit-2-unbranding/final/cwd-unbranding.js
@@ -0,0 +1,3 @@
+let targetElement = document.quearySelector ("SHEIN Clothes 1.png"); 
+console.log (targetElement.outerHTML); 
+targetElement.addEVENTListener ("mouseover", function () { console.log, (this.intertext); });
